6680116d7a9e358d10f516cd20fe6b8d1bf98f96
lb19082004 <leurybrand@gmail.com>
2026-03-10 22:52:10 -0400

    Agenda React

--- server.js
+++ server.js
@@ -4,27 +4,21 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 
-// 🔥 AQUÍ ACTIVAMOS CORS
-app.use(cors({
-    origin: "http://localhost:5500"
-}));
+
+app.use(cors());
+
 
 let contactos = [];
 
-// GET - Obtener contactos
+
 app.get("/contactos", (req, res) => {
     res.json(contactos);
 });
 
-// POST - Agregar contacto
+
 app.post("/contactos", (req, res) => {
-    const nuevo = req.body;
-    contactos.push(nuevo);
-
-    console.log("Contactos actuales:");
-    console.log(contactos); // 👈 Agrega esto
-
-    res.json({ mensaje: "Contacto agregado" });
+    contactos.push(req.body);
+    res.json({mensaje:"Contacto agregado"});
 });
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000");
